7cf01e5392e3f8581959e8132955f58b078670a0
suvamdawn94 <suvamdawn94@gmail.com>
2018-12-31 17:31:08 +0530

    add required npm list

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
+/*npm install --save firebase-functions firebase-admin express consolidate ejs firestore-store*/
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express= require('express');
+const engines= require('consolidate');
+const ejs =require('ejs');
+admin.initializeApp(functions.config().firebase);
+var db = admin.firestore();
+const app=express();
+app.set("view engine","ejs");
+app.engine('ejs', ejs.__express);
+app.set('views', './views');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+app.get('/',(req,res)=>{
+  res.send("Test");
+});
+
+exports.index = functions.https.onRequest(app);
